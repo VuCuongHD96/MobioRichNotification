78de4b0afbcba335cac087710ebb3fb6e468e052
cuongvx <cuongvx@mobio.vn>
2022-07-25 09:49:31 +0700

    create lib

---
+++ Source/Calculator.swift
@@ -0,0 +1,15 @@
+//
+//  Calculator.swift
+//  MobioRichNotification
+//
+//  Created by Sun on 25/07/2022.
+//
+
+import Foundation
+
+struct Calculator {
+    
+    static func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
