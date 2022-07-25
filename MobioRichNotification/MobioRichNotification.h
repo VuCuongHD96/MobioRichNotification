eabf4cd0fa6be34f039d6c6bc68cdd537991e044
cuongvx <cuongvx@mobio.vn>
2022-07-25 09:42:52 +0700

    Initial Commit

---
+++ MobioRichNotification/MobioRichNotification.h
@@ -0,0 +1,18 @@
+//
+//  MobioRichNotification.h
+//  MobioRichNotification
+//
+//  Created by Sun on 25/07/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MobioRichNotification.
+FOUNDATION_EXPORT double MobioRichNotificationVersionNumber;
+
+//! Project version string for MobioRichNotification.
+FOUNDATION_EXPORT const unsigned char MobioRichNotificationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobioRichNotification/PublicHeader.h>
+
+
