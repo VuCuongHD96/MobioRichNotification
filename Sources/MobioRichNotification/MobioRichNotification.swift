6497cabca09b02d663c68869e4c6a726e06ca0c9
cuongvx <cuongvx@mobio.vn>
2022-08-22 10:05:08 +0700

    create swift pakage

---
+++ Sources/MobioRichNotification/MobioRichNotification.swift
@@ -0,0 +1,6 @@
+public struct MobioRichNotification {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/MobioRichNotificationTests/MobioRichNotificationTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MobioRichNotification
+
+final class MobioRichNotificationTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MobioRichNotification().text, "Hello, World!")
+    }
+}
